13973d75434ef7c202d8cb68ec24c35b34c560f1
dmytro.laskuryk <dimonsensey@gmail.com>
2020-05-29 20:52:11 +0300

    create IoT

---
+++ ZooApiService/ZooApiService.IoT/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+
+namespace ZooApiService.IoT
+{
+    class Program
+    {
+        private const string ApiUrl = "";
+        private const int EmulatedId = 123;
+
+        static void Main(string[] args)
+        {
+            using var httpClient = new HttpClient();
+
+            var request = new
+            {
+                Id = EmulatedId,
+                Message = "Animal is Ok"
+            };
+
+            var stringContent = new StringContent(JsonConvert.SerializeObject(request), Encoding.UTF8, "application/json");
+
+            httpClient.PostAsync(ApiUrl, stringContent).GetAwaiter().GetResult();
+        }
+    }
+}
