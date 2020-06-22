132367865f151aa5954dba7789c159d819f06c61
Vladislav Bayrak <vlad.bay1rak@gmail.com>
2020-06-22 10:04:31 +0300

    test changes

---
+++ ZooApiService/ZooApiService.DAL.Data/Entities/AnimalDetails.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZooApiService.DAL.Data.Entities
+{
+    public class AnimalDetails
+    {
+        public int AnimalDetailsId { get; set; }
+
+        public double Weight { get; set; }
+
+        public double Height { get; set; }
+
+        public double BodyLength { get; set; }
+    }
+}
