ee8c34d8fdf3b2f8355702c0cbe888cf56d6c2d3
Dmytro Laskuryk <dimonsensey@gmail.com>
2020-08-26 20:37:51 +0300

    fix relative path

--- ZooApiService/ZooApiService.API/Startup.cs
+++ ZooApiService/ZooApiService.API/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 using System.IO;
 using ZooApiService.API.Configuration;
@@ -64,13 +65,16 @@ namespace ZooApiService.API
 
                 if (context.Response.StatusCode == 404 && !Path.HasExtension(context.Request.Path.Value))
                 {
-                    context.Request.Path = "/SPA/index.html";
+                    context.Request.Path = "/index.html";
                     await next();
                 }
             });
 
             app.UseDefaultFiles();
-            app.UseStaticFiles();
+            app.UseStaticFiles(new StaticFileOptions 
+            {
+                FileProvider = new PhysicalFileProvider(Path.Combine(env.WebRootPath, "SPA"))
+            });
 
             app.UseForwardedHeaders(new ForwardedHeadersOptions
             {
